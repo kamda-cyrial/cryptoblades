5bc3271a772e0132b4b78e9197121f14c0f64361
Kacper <kacpertokarczyk1@gmail.com>
2021-07-09 11:23:48 +0200

    Allow private trades on market

--- frontend/src/store.ts
+++ frontend/src/store.ts
@@ -299,6 +299,10 @@ export function createStore(web3: Web3) {
       getIsInCombat(state: IState): boolean {
         return state.isInCombat;
       },
+
+      getWalletAddress(state): string {
+        return state.defaultAccount || '';
+      },
     },
 
     mutations: {
@@ -1167,6 +1171,19 @@ export function createStore(web3: Web3) {
           .call(defaultCallOptions(state));
       },
 
+      async fetchMarketNftTargetBuyer({ state }, { nftContractAddr, tokenId }) {
+        const { NFTMarket } = state.contracts();
+        if(!NFTMarket) return;
+
+        // returns the listing's target buyer address
+        return await NFTMarket.methods
+          .getTargetBuyer(
+            nftContractAddr,
+            tokenId
+          )
+          .call(defaultCallOptions(state));
+      },
+
       async fetchMarketTax({ state }, { nftContractAddr }) {
         const { NFTMarket } = state.contracts();
         if(!NFTMarket) return;
@@ -1193,7 +1210,8 @@ export function createStore(web3: Web3) {
           .call(defaultCallOptions(state));
       },
 
-      async addMarketListing({ state, dispatch }, { nftContractAddr, tokenId, price }: { nftContractAddr: string, tokenId: string, price: string }) {
+      async addMarketListing({ state, dispatch }, { nftContractAddr, tokenId, price, targetBuyer }:
+      { nftContractAddr: string, tokenId: string, price: string, targetBuyer: string }) {
         const { NFTMarket, Weapons, Characters } = state.contracts();
         if(!NFTMarket || !Weapons || !Characters) return;
 
@@ -1207,7 +1225,7 @@ export function createStore(web3: Web3) {
           .send(defaultCallOptions(state));
 
         const res = await NFTMarket.methods
-          .addListing(nftContractAddr, tokenId, price)
+          .addListing(nftContractAddr, tokenId, price, targetBuyer)
           .send({
             from: state.defaultAccount,
           });
@@ -1241,6 +1259,25 @@ export function createStore(web3: Web3) {
         } = res.events.ListingPriceChange.returnValues;
 
         return { seller, nftID, newPrice } as { seller: string, nftID: string, newPrice: string };
+      },
+
+      async changeMarketListingTargetBuyer({ state }, { nftContractAddr, tokenId, newTargetBuyer }:
+      { nftContractAddr: string, tokenId: string, newTargetBuyer: string }) {
+        const { NFTMarket } = state.contracts();
+        if(!NFTMarket) return;
+
+        const res = await NFTMarket.methods
+          .changeListingTargetBuyer(nftContractAddr, tokenId, newTargetBuyer)
+          .send({
+            from: state.defaultAccount,
+          });
+
+        const {
+          seller,
+          nftID
+        } = res.events.ListingTargetBuyerChange.returnValues;
+
+        return { seller, nftID, newTargetBuyer } as { seller: string, nftID: string, newTargetBuyer: string };
       },
 
       async cancelMarketListing({ state, dispatch }, { nftContractAddr, tokenId }: { nftContractAddr: string, tokenId: string }) {
